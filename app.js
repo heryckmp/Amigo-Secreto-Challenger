14afa0a2eac696737a75c7bab6e020df1e2b8337
erick <heryckx5@gmail.com>
2025-01-22 02:02:15 -0300

    Exibe alerta caso o nome seja vazio

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let amigos = ['mirela', 'lucas', 'Eric','Paola']
+let amigos = ['mirela', 'lucas', 'Eric', 'Paola'];
+
+function adicionarAmigo() {
+    let input = document.getElementById('amigo');
+    let nome = input.value.trim();  //Remove espaços em branco extras para nao afetar no processamento do valor
+}
+if (nome == '') {
+    alert('Por favor, insira um nome:');
+    return;
+}
